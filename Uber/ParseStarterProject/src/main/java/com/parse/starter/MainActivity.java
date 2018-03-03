1464691c231fe1855bc75a264d8a1e4e3838d05e
Divya <you@example.com>
2018-03-03 14:36:51 +0530

    initial commit

---
+++ Uber/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,45 @@
+
+package com.parse.starter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity  {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+  }
+
+
+}
